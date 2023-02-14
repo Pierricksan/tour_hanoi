d78517d4d6bba6c47a326c3603fc7d6498d306c9
Pierrick Rousselot <rousselot.pierrick@gmail.com>
2023-02-14 16:19:24 +0100

    ajout fonctionnalité afficher nombre de tour nécessaire

--- hanoi.js
+++ hanoi.js
@@ -92,26 +92,35 @@ A = 0 && C = 3 = Z
 
 */
 let nbDisques = prompt('Entrez le nombre de disques :')
+
+
+function nbrTour(n) {
+    return nbrTour = 2**n -1
+ }
+
 function deplacerDisque(pileDepart, pileArrivee) {
   pileArrivee.push(pileDepart.pop())
 }
+
 function tourHanoi(n, depart, arrivee, intermediaire) {
   if (n > 0) {
     tourHanoi(n - 1, depart, intermediaire, arrivee)
-    console.log(
-      'Déplacer le disque ' + n + ' de ' + depart + ' vers ' + arrivee,
-    )
+    console.log('Déplacer le disque ' + n + ' de ' + depart + ' vers ' + arrivee,)
     deplacerDisque(eval(depart), eval(arrivee))
-
-    console.log('Pile A : ' + A + ', Pile B : ' + B + ', Pile C : ' + C)
+    console.log('Pile A : ' + A.length + ', Pile B : ' + B.length + ', Pile C : ' + C.length)
     tourHanoi(n - 1, intermediaire, arrivee, depart)
   }
 } // Initialiser les piles A, B, C avec le nombre de disques spécifié
 let A = []
+
 for (let i = nbDisques; i >= 1; i--) {
-  A.push(i)
-}
+        A.push(i)
+    }
+
 let B = []
 let C = []
 tourHanoi(nbDisques, 'A', 'C', 'B')
+
+let mustTurn = nbrTour(nbDisques)
+console.log(`Le nombre de tour nécessaire pour ${nbDisques} est de ${mustTurn}`)
 
