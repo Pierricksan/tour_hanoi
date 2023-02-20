a29d095bd6eeb3cefec788481c22f7de3d6d1c00
Rousselot <rousselot.pierrick@gmail.com>
2023-02-20 10:00:10 +0100

    Ajout et modification pour fichier iteratif

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tour_hanoi
+
+Bienvenue dans l'exercice de la tour 
+
+pour faire appelle aux différentes fonctions, ne pas oublier de changer l'appel du fichier dans l'index.html
+
+opérationnelle : 
+- récursif : RAS  
+- itératif : fonctionne pour 3 palets, au delà les conditions nécessaires n'ont pas réussi à être mise en place
--- hanoi-iteratif.js
+++ hanoi-iteratif.js
@@ -1,1 +1,51 @@
 // faire le programme hanoi exclusivement en itératif
+// Demander le nombre de palets à l'utilisateur
+var nbPalets = prompt("Entrez le nombre de palets :");
+
+// Convertir le nombre de palets en nombre entier
+nbPalets = parseInt(nbPalets);
+
+// Vérifier que le nombre de palets est un nombre valide
+if (isNaN(nbPalets) || nbPalets < 1) {
+  alert("Le nombre de palets doit être un nombre entier positif !");
+} else {
+  // Initialiser les piliers et le nombre de tours
+  var p1 = [], p2 = [], p3 = [];
+  var tour = 1;
+
+  // Remplir le premier pilier avec les palets
+  for (var i = nbPalets; i > 0; i--) {
+    p1.push(i);
+  }
+
+  // Afficher l'état initial des piliers
+  console.log("Tour " + tour + " :");
+  console.log("Pilier 1 : " + p1.join(" "));
+  console.log("Pilier 2 : " + p2.join(" "));
+  console.log("Pilier 3 : " + p3.join(" "));
+
+  // Boucle itérative pour résoudre le jeu
+  var source = p1, destination = p3, auxiliaire = p2;
+  while (destination.length < nbPalets) {
+    tour++;
+    if (nbPalets % 2 === 0) {
+      // Échanger les piliers source et auxiliaire
+      var temp = source;
+      source = auxiliaire;
+      auxiliaire = temp;
+    }
+    // Déplacer un palet du pilier source au pilier destination
+    destination.push(source.pop());
+    console.log("Tour " + tour + " :");
+    console.log("Pilier 1 : " + p1.join(" "));
+    console.log("Pilier 2 : " + p2.join(" "));
+    console.log("Pilier 3 : " + p3.join(" "));
+    // Échanger les piliers destination et auxiliaire
+    var temp = destination;
+    destination = auxiliaire;
+    auxiliaire = temp;
+  }
+
+  // Afficher le nombre de tours effectuées pour résoudre le jeu
+  console.log("Le jeu est terminé en " + tour + " tours.");
+}
